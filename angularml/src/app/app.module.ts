2322fef62bf50650c884f608aed0663f1351a672
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-28 06:19:28 +0100

    Upload FE

--- angularml/src/app/app.module.ts
+++ angularml/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule} from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CtscanComponent } from './ctscan/ctscan.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{  MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,12 @@ import { CtscanComponent } from './ctscan/ctscan.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+  MatIconModule, MatCardModule, 
+  MatButtonModule, MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularml/src/app/ctscan/ctscan.component.ts
+++ angularml/src/app/ctscan/ctscan.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CtscanComponent implements OnInit {
 
+  name: string;
+  image:File;
+  
+
   constructor() { }
 
   ngOnInit(): void {
