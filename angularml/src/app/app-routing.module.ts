6a95780cb254763294d83897891643e3efdb6f4f
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-28 04:37:55 +0100

    ctscan routing complete

--- angularml/src/app/app-routing.module.ts
+++ angularml/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CtscanComponent } from './ctscan/ctscan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'images', component:CtscanComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
