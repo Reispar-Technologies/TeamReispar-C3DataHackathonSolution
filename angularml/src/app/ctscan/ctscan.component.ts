ae62cd3fd76326f43cbeb05da7108ff347a99e60
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-29 13:51:21 +0100

    Image upload:with feed back

--- angularml/src/app/ctscan/ctscan.component.ts
+++ angularml/src/app/ctscan/ctscan.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { map, catchError } from "rxjs/operators"
+import { throwError } from "rxjs";
 
 @Component({
   selector: 'app-ctscan',
@@ -9,31 +11,49 @@ import { HttpClient } from '@angular/common/http';
 export class CtscanComponent implements OnInit {
 
   name: string;
-  image:File;
-  
+  image: File;
+  progress: number;
+  message:string;
 
-  constructor(private http:HttpClient) { }
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
-  onTextchange(event:any){
+  onTextchange(event: any) {
     this.name = event.target.value;
   }
 
-  onImagechange(event:any){
+  onImagechange(event: any) {
     this.image = event.target.files[0];
   }
-  
-  newImage(){
+
+  newImage() {
     const uploadImage = new FormData;
+    this.progress = 1;
     uploadImage.append('name', this.name);
     uploadImage.append('image', this.image, this.image.name);
-    this.http.post("http://127.0.0.1:8000/image/", uploadImage).subscribe(
-      data => console.log(data), 
-      error => console.log(error)
+    this.http.post("http://127.0.0.1:8000/image/", uploadImage, {
+      reportProgress: true,
+      observe: "events"
+    }).pipe(
+      map((event: any) => {
+        if (event.type == HttpEventType.UploadProgress) {
+          this.progress = Math.round((100 / event.total) * event.loaded);
+          if (this.progress === 100){
+            this.message = "Upload Completed"
+          }
+        } else if (event.type == HttpEventType.Response) {
+          this.progress = null;
+        }
+      }),
+      catchError((err: any) => {
+        this.progress = null;
+        this.message = "Upload failed"
+        return throwError(err.message);
+      })
+    ).toPromise();
 
-      
-      
-    )
   }
 }
